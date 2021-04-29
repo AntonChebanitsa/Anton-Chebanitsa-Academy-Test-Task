e119b6c1257070172d8d0979f61261a87f74b8ec
Anton <antob.chebanitsa@gmail.com>
2021-04-29 18:21:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anton_Chebanitsa_Academy_Test_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,10 @@
+﻿namespace Anton_Chebanitsa_Academy_Test_Task
+{
+    public class Rover
+    {
+        public static void CalculateRoverPath(int[,] map)
+        {
+
+        }
+    }
+}
