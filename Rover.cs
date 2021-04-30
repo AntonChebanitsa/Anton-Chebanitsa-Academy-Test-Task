2b9760d0a3a187c54437da715ac0ed4bca208c3e
Anton <antob.chebanitsa@gmail.com>
2021-04-30 21:45:53 +0300

    Class MyPoint moved to Rover file

--- Rover.cs
+++ Rover.cs
@@ -6,6 +6,19 @@ using System.Linq;
 
 namespace Anton_Chebanitsa_Academy_Test_Task
 {
+    public class MyPoint
+    {
+        public Point Position { get; set; }
+
+        public int FullPathLength { get; set; }
+
+        public MyPoint PrewPoint { get; set; }
+
+        public int HeuristicPathLength { get; set; }
+
+        public int EstimatePath => FullPathLength + HeuristicPathLength;
+    }
+
     public class Rover
     {
         private static void Main()
